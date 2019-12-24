5ccd7decaadaa8c53975ceae7fdf959323b577cd
baemincheon <baemincheon@gmail.com>
2019-12-24 20:44:49 +0900

    create sandbox project with 3rd person template

---
+++ ProjectSandbox/Source/ProjectSandbox/ProjectSandbox.Build.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectSandbox : ModuleRules
+{
+	public ProjectSandbox(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
