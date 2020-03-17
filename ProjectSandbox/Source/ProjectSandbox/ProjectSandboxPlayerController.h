d5023a40eb540b133913802da56397f4e3dba7ce
unknown <baemincheon@gmail.com>
2020-03-18 01:27:08 +0900

    add unique pointer test code

--- ProjectSandbox/Source/ProjectSandbox/ProjectSandboxPlayerController.h
+++ ProjectSandbox/Source/ProjectSandbox/ProjectSandboxPlayerController.h
@@ -6,6 +6,34 @@
 #include "GameFramework/PlayerController.h"
 #include "ProjectSandboxPlayerController.generated.h"
 
+class UserInfo
+{
+public:
+	UserInfo(int32 NewUserID, char* NewUserName)
+		: UserID(NewUserID), UserName(NewUserName), bInitialized(false)
+	{
+		__debugbreak();
+
+		bInitialized = true;
+	}
+	virtual ~UserInfo()
+	{
+		__debugbreak();
+
+		if (UserName != nullptr)
+		{
+			delete[] UserName;
+		}
+
+		bInitialized = false;
+	}
+
+private:
+	int32 UserID;
+	char* UserName;
+	bool bInitialized;
+};
+
 /**
  * 
  */
@@ -16,11 +44,16 @@ class PROJECTSANDBOX_API AProjectSandboxPlayerController : public APlayerControl
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 private:
 	void ShowWelcomeText();
+	void CreateUserInformation();
+	void ReleaseUserInformation();
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TSubclassOf<class UUserWidget> WelcomeText;
+
+	TUniquePtr<class UserInfo> UserInformation;
 };
